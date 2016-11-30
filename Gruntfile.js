75b5d3bd57781b6d0ebcba810e278b7f51af72c8
Željko Filipin <zeljko.filipin@gmail.com>
2016-11-30 13:57:28 +0100

    Set up Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,12 @@
+module.exports = function ( grunt ) {
+
+	require( 'load-grunt-tasks' )( grunt );
+
+	// Project configuration.
+	grunt.initConfig( {
+	} );
+
+	// Default task(s).
+	grunt.registerTask( 'default', [] );
+
+};
