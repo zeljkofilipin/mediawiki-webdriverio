259c2561629ba2fda77560154b5eab8b4deda8b0
Željko Filipin <zeljko.filipin@gmail.com>
2016-11-30 15:57:10 +0100

    Removed unneeded trailing slash from baseUrl

--- wdio.conf.js
+++ wdio.conf.js
@@ -68,7 +68,7 @@ exports.config = {
 	//
 	// Set a base URL in order to shorten url command calls. If your url parameter starts
 	// with "/", then the base url gets prepended.
-	baseUrl: 'http://127.0.0.1:8080/wiki/',
+	baseUrl: 'http://127.0.0.1:8080/wiki',
 	//
 	// Default timeout for all waitFor* commands.
 	waitforTimeout: 10000,
