859e242b51b3459c01d522c8547f06c450a3d48e
Željko Filipin <zeljko.filipin@gmail.com>
2016-11-30 16:04:40 +0100

    Page should be edited

--- test/page.js
+++ test/page.js
@@ -1,8 +1,16 @@
 /* global browser */
 var assert = require( 'assert' );
+
 describe( 'Page', function () {
+
 	it( 'should be created', function () {
 		browser.url( '/Does_not_exist' );
 		assert( browser.isVisible( 'li#ca-edit a' ) );
 	} );
+
+	it( 'should be edited', function () {
+		browser.url( '/Main_Page' );
+		assert( browser.isVisible( 'li#ca-edit a' ) );
+	} );
+
 } );
